1a41fccea061fd3fa2df0564d065a093a9ef1f22
Kuklina <kuklina.aleksandra@rambler.ru>
2021-04-12 20:22:18 +0500

    Добавьте файлы проекта.

---
+++ Lesson4-02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lesson4_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числа для суммирования через пробел");
+            Console.WriteLine(GetSum(Console.ReadLine()));
+            Console.WriteLine();
+
+        }
+
+        static int GetSum(string inputline)
+        {
+            int sum = 0;
+            string numbers = " ";
+            for (int i = 0; i <= inputline.Length; i++)
+            {
+                if (i == inputline.Length || inputline[i] == ' ')
+                {
+                    if (numbers != "")
+                    {
+                        sum += Convert.ToInt32(numbers);
+                        numbers = "";
+                    }
+                }
+                else
+                {
+                    numbers += inputline[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
